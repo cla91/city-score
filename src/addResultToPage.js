ed8763f6006bbfa15b1453e6da17bfc366f147b2
cla91 <83760330+cla91@users.noreply.github.com>
2023-03-14 09:34:53 +0100

    Add files via upload

--- src/addResultToPage.js
+++ src/addResultToPage.js
@@ -1,6 +1,7 @@
 import fetchUrl from "./fetchUrl";
 import createCategoryElement from "./createCategoryElement";
-
+import get from "lodash/get";
+import animation from "./animation";
 const cityTitleName = document.querySelector("[data-city-name]");
 const citySummary = document.querySelector("[data-city-summary]");
 const totalScoreProgressBar = document.querySelector(
@@ -10,19 +11,18 @@ const totalScoreValue = document.querySelector("[data-total-score-value]");
 const categoriesList = document.querySelector("[data-categories-list]");
 const header = document.querySelector("[data-header]");
 const main = document.querySelector("[data-main]");
+
 export default function addResultToPage(cityName, cityUrl) {
   fetchUrl(`${cityUrl}scores/`).then((city) => {
-    const categories = city.categories;
-    const summary = city.summary
+    const categories = get(city, "categories");
+    const summary = get(city, "summary")
       .replace(/<b>/g, "<em>")
       .replace(/<\/b>/g, "</em>");
-    const cityScore = city.teleport_city_score.toFixed(2);
+    const cityScore = get(city, "teleport_city_score").toFixed(2);
 
     cityTitleName.textContent = cityName;
     citySummary.innerHTML = summary;
-    totalScoreProgressBar.style.background = `conic-gradient(hsl(var(--primary-color)) ${
-      Math.round(cityScore) * 3.6
-    }deg, #fff 0deg`;
+    animation(totalScoreProgressBar, Math.round(cityScore) * 3.6);
     totalScoreValue.textContent = `${cityScore}%`;
 
     categoriesList.textContent = "";
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const RemovePlugin = require("remove-files-webpack-plugin");
 
 module.exports = {
   entry: "./src/main.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    //path: path.resolve(__dirname, "dist")
   },
+  watch: true,
 };
